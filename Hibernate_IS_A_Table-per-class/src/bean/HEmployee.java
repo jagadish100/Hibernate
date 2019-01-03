c94903a6debeaedc34903a0b619a4ae8dd276000
Jagadish Somisetty <Jagadish Somisetty@Jaga>
2019-01-03 09:00:56 +0530

    First

---
+++ Hibernate_IS_A_Table-per-class/src/bean/HEmployee.java
@@ -0,0 +1,27 @@
+package bean;
+
+public class HEmployee extends Employee{
+
+	private int wh;
+	
+	public HEmployee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public HEmployee(int id, String name, String email, int salary, int wh) {
+		super(id, name, email, salary);
+		this.wh = wh;
+	}
+
+	public int getWh() {
+		return wh;
+	}
+
+	public void setWh(int wh) {
+		this.wh = wh;
+	}
+
+	
+	
+	
+}
