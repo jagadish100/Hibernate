4326d0886515eb7de8746c1adfa85b130c219f09
Jagadish Somisetty <Jagadish Somisetty@Jaga>
2019-01-03 09:00:28 +0530

    First

---
+++ Hibernate_IS_A_Table-per-concreteClass/src/bean/Admin.java
@@ -0,0 +1,27 @@
+package bean;
+
+public class Admin extends Employee{
+
+	private String branchname;
+	
+	public Admin() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public Admin(int id, String name, String email, int salary, String branchname) {
+		super(id, name, email, salary);
+		this.branchname = branchname;
+	}
+
+
+	public String getBranchname() {
+		return branchname;
+	}
+
+	public void setBranchname(String branchname) {
+		this.branchname = branchname;
+	}
+	
+	
+}
